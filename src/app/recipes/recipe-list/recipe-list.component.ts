d2160533ac2be1d474107760f5220b7f95c2e07d
Zak <zakary.howard94@gmail.com>
2018-12-03 15:54:25 +0100

    bugfix with recipe model failing to take an argument, hardcoded recipe list to see how it displays

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
+
 @Component({
   selector: '[app-recipe-list]',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.sass']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.jocooks.com/wp-content/uploads/2014/03/sweet-and-sour-chicken.jpg')
+  ];
 
   constructor() { }
 
