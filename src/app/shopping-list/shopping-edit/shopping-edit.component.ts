0186a6335a8e98e77cc2afa00f368468ed7dffc4
Zak <zakary.howard94@gmail.com>
2020-01-20 20:25:46 +0100

    updates

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { Ingredient } from 'src/app/shared/ingredient.model';
-import { ShoppingListService } from '../shopping-list.service';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { Ingredient } from "src/app/shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list.service";
 
 @Component({
-  selector: '[app-shopping-edit]',
-  templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.sass']
+  selector: "[app-shopping-edit]",
+  templateUrl: "./shopping-edit.component.html",
+  styleUrls: ["./shopping-edit.component.sass"]
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @ViewChild("nameInput") nameInputRef: ElementRef;
+  @ViewChild("amountInput") amountInputRef: ElementRef;
 
   constructor(private shoppingListService: ShoppingListService) {}
 
